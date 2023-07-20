1269bc3f0782d4fd7f79e67eebcb51e63b58b47e
Aditya Chourey <adityachourey@Adityas-MacBook-Pro.local>
2023-07-20 12:41:52 +0530

    routerAdded

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const userRouter = require('./api/users/user.router')
+const questionRouter = require('./api/Questions/questions.router')
 
 app.use(express.json())
+app.use('/api/users', userRouter)
 app.use('/api/users', userRouter)
 
 app.listen(process.env.APP_PORT, () => {
